f9f68b78e40a50beaa93bbbc08fc51bdbccb1327
Nicole Durrant-Eunson <ndurrant-eunson@rrc.ca>
2026-02-23 11:43:38 -0600

    Added the appscan extention

--- assignment1_code_sample_2.ts
+++ assignment1_code_sample_2.ts
@@ -6,7 +6,7 @@ import * as http from 'http';
 const dbConfig = {
     host: 'mydatabase.com',
     user: 'admin',
-    password: 'secret123',
+    password: 'secret123678dddd9',
     database: 'mydb'
 };
 
